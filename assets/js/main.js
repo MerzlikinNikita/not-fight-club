80cbaafa74a86fcb7692b9a1c1dca9f9bd1addcb
Merzlikin Nikita <nikmerz2002@mail.ru>
2025-08-17 19:02:45 +0600

    feat: add settings functional

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,11 +27,11 @@ const urlRoute = (event) => {
 const urlLocationHandler = async () => {
   let location = window.location.pathname;
 
-  // if (!location.startsWith("/")) {
-  //   location = "/" + location;
-  // }
+  if (location === "/index.html") {
+    location = "/";
+  }
 
-  if (location !== "/registration" && !localStorage.getItem("regName")) {
+  if (location !== "/registration" && !localStorage.getItem("playerName")) {
     location = "/registration";
   }
 
@@ -40,14 +40,14 @@ const urlLocationHandler = async () => {
 
   document.getElementById("content").innerHTML = html;
 
-  const playerNames = document.querySelectorAll(".player__name");
-  playerNames.forEach(
-    (name) => (name.textContent = localStorage.getItem("regName"))
-  );
-
   if (location === "/registration") {
     registrationActions();
+  } else if (location === "/settings") {
+    settingsActions();
   }
+
+  const playerName = document.querySelector(".player__name");
+  playerName.textContent = localStorage.getItem("playerName");
 };
 
 const registrationActions = () => {
@@ -55,15 +55,57 @@ const registrationActions = () => {
   const regName = document.getElementById("user-name");
 
   regBtn.addEventListener("click", function () {
-    if (regName.value !== "") {
-      localStorage.setItem("regName", regName.value);
+    const newPlayerName = regName.value.trim();
+
+    if (newPlayerName) {
+      localStorage.setItem("playerName", newPlayerName);
       window.history.pushState({}, "", "/");
       urlLocationHandler();
     }
   });
 };
 
-// const settingsActions = () => {};
+const settingsActions = () => {
+  const editNameBtn = document.getElementById("edit-name-btn");
+  const playerName = document.querySelector(".player__name");
+  const changeNameInput = document.getElementById("edit-name-input");
+  const saveNameBtn = document.getElementById("save-name-btn");
+
+  editNameBtn.addEventListener("click", () => {
+    playerName.style.display = "none";
+    editNameBtn.style.display = "none";
+    changeNameInput.style.display = "inline-block";
+    saveNameBtn.style.display = "inline-block";
+
+    changeNameInput.value = playerName.textContent;
+  });
+
+  changeNameInput.addEventListener("input", () => {
+    const newPlayerName = changeNameInput.value.trim();
+
+    if (!newPlayerName) {
+      saveNameBtn.disabled = true;
+      saveNameBtn.style.cursor = "not-allowed";
+    } else {
+      saveNameBtn.disabled = false;
+      saveNameBtn.style.cursor = "default";
+    }
+  });
+
+  saveNameBtn.addEventListener("click", () => {
+    const newPlayerName = changeNameInput.value.trim();
+
+    if (newPlayerName) {
+      playerName.textContent = newPlayerName;
+      localStorage.setItem("playerName", newPlayerName);
+    }
+
+    playerName.style.display = "inline-block";
+    editNameBtn.style.display = "inline-block";
+    changeNameInput.style.display = "none";
+    saveNameBtn.style.display = "none";
+  });
+};
 
 window.onpopstate = urlLocationHandler;
 window.route = urlRoute;
